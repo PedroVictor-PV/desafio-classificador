e27f344ec1c9549a67cbd7f5c03886d1645ae8da
PedroVictor-PV <pedro.cavalcante0515@gmail.com>
2024-09-23 15:32:56 -0300

    classificador de heroi

---
+++ classificador.js
@@ -0,0 +1,20 @@
+let nomeHeroi = "Pedro"
+let exp = 10001
+
+
+if(exp<=1000){
+    console.log("O herói " + nomeHeroi + " é de nível ferro");
+}  else if(exp>=1001 && exp<=2000){
+    console.log ("O herói " + nomeHeroi + " é de nível bronze")}
+    else if(exp>=2001 && exp<=5000){
+        console.log ("O herói " + nomeHeroi + " é de nível prata")}
+    else if(exp>=5001 && exp<=7000){
+        console.log ("O herói " + nomeHeroi + " é de nível ouro")}
+    else if(exp>=7001 && exp<=8000){
+        console.log ("O herói " + nomeHeroi + " é de nível platina")}   
+   else if(exp>=8001 && exp<=9000){
+         console.log ("O herói " + nomeHeroi + " é de nível ascendente")} 
+   else if(exp>=9001 && exp<=10000){
+        console.log ("O herói " + nomeHeroi + " é de nível Imortal")}
+    else if(exp>=10001){
+        console.log ("O herói " + nomeHeroi + " é de nível Radiante")} 
